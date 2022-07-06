c9083cb6234b99d8d4bfbd414685574fb860d00d
Mehmet Yıldırım <mehmetyildirim.developer@gmail.com>
2022-07-06 21:46:34 +0300

    feat: add movement system

---
+++ Assets/Scripts/MovementCube.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementCube : MonoBehaviour
+{
+    [SerializeField] float ForwardSpeed;
+    [SerializeField] float HorizontalSpeed;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Horizontal = Input.GetAxis("Horizontal")*HorizontalSpeed*Time.deltaTime;
+        this.transform.Translate(Horizontal,0, ForwardSpeed * Time.deltaTime);
+
+
+    }
+}
